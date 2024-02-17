188ea81d3afb4ade5c76b7504d9ae72919c05602
Laura Haro Escoi <lauraha.escoi@gmail.com>
2024-02-17 19:09:07 +0100

    fixed user schema

--- front_walleps/lib/screens/profile/profile_screen.dart
+++ front_walleps/lib/screens/profile/profile_screen.dart
@@ -31,113 +31,116 @@ class ProfileScreen extends StatelessWidget {
             return Center(child: Text("Error al obtener datos del usuario"));
           } else if (snapshot.hasData) {
             User user = snapshot.data!;
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 32),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SizedBox(width: 16),
-                      CircleAvatar(
-                        radius: 48,
-                        backgroundImage: user.image != null
-                            ? NetworkImage(user.image!)
-                            : AssetImage('assets/default_image.png')
-                                as ImageProvider,
-                      ),
-                      SizedBox(width: 16),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(user.name,
-                                style: Theme.of(context).textTheme.titleLarge),
-                            Text(user.email,
-                                style: Theme.of(context).textTheme.bodyLarge),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 32),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: Text('Active Contracts',
-                        style: Theme.of(context).textTheme.titleMedium),
-                  ),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
+            return SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 32),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Card.CardWidget(
-                          size: Card.CardSize.small,
-                          color: Card.CardColor.secondary,
-                          iconData: Icons.phone,
-                          title: 'Phone',
-                          subtitle: 'Service Provider: Orange',
-                          onPressed: () {},
-                          child: Text(
-                              'Monthly Average: XX.XX.€.\nNext Billing Date: DD/MM/YYYY'),
+                        SizedBox(width: 16),
+                        CircleAvatar(
+                          radius: 48,
+                          backgroundImage: user.image != null
+                              ? NetworkImage(user.image!)
+                              : AssetImage('assets/default_image.png')
+                                  as ImageProvider,
                         ),
-                        Card.CardWidget(
-                          size: Card.CardSize.small,
-                          color: Card.CardColor.secondary,
-                          iconData: Icons.lightbulb,
-                          title: 'Electricity',
-                          subtitle: 'Utility Company: Endesa',
-                          onPressed: () {},
-                          child: Text(
-                              'Monthly Average: XX.XX.€.\nNext Billing Date: DD/MM/YYYY.'),
-                        ),
-                        Card.CardWidget(
-                          size: Card.CardSize.small,
-                          color: Card.CardColor.secondary,
-                          iconData: Icons.water_drop,
-                          title: 'Water Service',
-                          subtitle: 'Water Supplier: Aigües de Lleida',
-                          onPressed: () {},
-                          child: Text(
-                              'Monthly Average: XX.XX.€.\nNext Billing Date: DD/MM/YYYY.'),
+                        SizedBox(width: 16),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(user.name,
+                                  style:
+                                      Theme.of(context).textTheme.titleLarge),
+                              Text(user.email,
+                                  style: Theme.of(context).textTheme.bodyLarge),
+                            ],
+                          ),
                         ),
                       ],
                     ),
-                  ),
-                  SizedBox(height: 32),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: Text('Consume Planning',
-                        style: Theme.of(context).textTheme.titleMedium),
-                  ),
-                  //TODO: Insertar weeklyPlanning, si no hay weeklyPlanning hecho, mostrar un botón para ir al Planner
-                  //mostrar el schedule del usuario con el widget Scheduler
-                  SizedBox(height: 40),
-                  // Aquí insertamos el FutureBuilder para el horario del usuario
-                  FutureBuilder<List<DeviceSchedule>>(
-                    future: userService.getSchedulerByUserId(demoUserId,
-                        true), // Obtiene la programación del usuario
-                    builder: (context, scheduleSnapshot) {
-                      if (scheduleSnapshot.connectionState ==
-                          ConnectionState.waiting) {
-                        return CircularProgressIndicator();
-                      } else if (scheduleSnapshot.hasError) {
-                        return Text("Error al obtener la programación");
-                      } else if (scheduleSnapshot.hasData) {
-                        // Aquí se muestra el horario después de las tarjetas
-                        return WeeklySchedule(
-                            schedule: scheduleSnapshot.data!,
-                            homeHours: user.home_hours,
-                            homeDuration: user.home_duration);
-                      } else {
-                        return Text(
-                            "No se encontró la programación del usuario");
-                      }
-                    },
-                  ),
-                ],
+                    SizedBox(height: 32),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Text('Active Contracts',
+                          style: Theme.of(context).textTheme.titleMedium),
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          Card.CardWidget(
+                            size: Card.CardSize.small,
+                            color: Card.CardColor.secondary,
+                            iconData: Icons.phone,
+                            title: 'Phone',
+                            subtitle: 'Service Provider: Orange',
+                            onPressed: () {},
+                            child: Text(
+                                'Monthly Average: XX.XX.€.\nNext Billing Date: DD/MM/YYYY'),
+                          ),
+                          Card.CardWidget(
+                            size: Card.CardSize.small,
+                            color: Card.CardColor.secondary,
+                            iconData: Icons.lightbulb,
+                            title: 'Electricity',
+                            subtitle: 'Utility Company: Endesa',
+                            onPressed: () {},
+                            child: Text(
+                                'Monthly Average: XX.XX.€.\nNext Billing Date: DD/MM/YYYY.'),
+                          ),
+                          Card.CardWidget(
+                            size: Card.CardSize.small,
+                            color: Card.CardColor.secondary,
+                            iconData: Icons.water_drop,
+                            title: 'Water Service',
+                            subtitle: 'Water Supplier: Aigües de Lleida',
+                            onPressed: () {},
+                            child: Text(
+                                'Monthly Average: XX.XX.€.\nNext Billing Date: DD/MM/YYYY.'),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 32),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Text('Consume Planning',
+                          style: Theme.of(context).textTheme.titleMedium),
+                    ),
+                    SizedBox(height: 40),
+                    // Aquí insertamos el FutureBuilder para el horario del usuario
+                    Center(
+                      child: FutureBuilder<List<DeviceSchedule>>(
+                        future: userService.getSchedulerByUserId(demoUserId,
+                            true), // Obtiene la programación del usuario
+                        builder: (context, scheduleSnapshot) {
+                          if (scheduleSnapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return CircularProgressIndicator();
+                          } else if (scheduleSnapshot.hasError) {
+                            return Text("Error al obtener la programación");
+                          } else if (scheduleSnapshot.hasData) {
+                            // Aquí se muestra el horario después de las tarjetas
+                            return WeeklySchedule(
+                                schedule: scheduleSnapshot.data!,
+                                homeHours: user.home_hours,
+                                homeDuration: user.home_duration);
+                          } else {
+                            return Text(
+                                "No se encontró la programación del usuario");
+                          }
+                        },
+                      ),
+                    ),
+                  ],
+                ),
               ),
             );
           } else {
