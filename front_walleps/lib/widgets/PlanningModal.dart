64d7c7f80f28ca1cb9cdffc78682c854eef5a70b
Laura Haro Escoi <lauraha.escoi@gmail.com>
2024-02-17 23:39:27 +0100

    changes schedule modal

--- front_walleps/lib/widgets/PlanningModal.dart
+++ front_walleps/lib/widgets/PlanningModal.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:front_walleps/theme/themes.dart';
+import '../../models/device_scheduler.dart';
+import '../../services/user_service.dart';
 import '../../widgets/Scheduler.dart';
-import '../models/device_scheduler.dart';
+import '../../theme/themes.dart';
+import '../../models/user.dart';
 
 class PlanningModal extends StatelessWidget {
+  final int userId = 1;
+
+  PlanningModal({Key? key}) : super(key: key);
+
+  final UserService userService = UserService();
+
   @override
   Widget build(BuildContext context) {
     return Dialog(
@@ -12,27 +20,93 @@ class PlanningModal extends StatelessWidget {
         side: BorderSide(color: AppThemes.tertiaryColor, width: 2.0),
       ),
       surfaceTintColor: AppThemes.lightBlackColor,
-      child: Center(
-        //TODO: Poner las variables correctas
-        /*
-        child: FutureBuilder<List<DeviceSchedule>>(
-          future: userService.getSchedulerByUserId(demoUserId, true),
-          builder: (context, scheduleSnapshot) {
-            if (scheduleSnapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            } else if (scheduleSnapshot.hasError) {
-              return Text("Error al obtener la programación");
-            } else if (scheduleSnapshot.hasData) {
-              return WeeklySchedule(
-                  schedule: scheduleSnapshot.data!,
-                  homeHours: user.home_hours,
-                  homeDuration: user.home_duration);
-            } else {
-              return Text("No se encontró la programación del usuario");
-            }
-          },
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Weekly Schedule',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            FutureBuilder<User>(
+              future: userService.getUser(userId),
+              builder: (context, snapshotUser) {
+                if (snapshotUser.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshotUser.hasError) {
+                  return Text("Error: ${snapshotUser.error}");
+                } else if (snapshotUser.hasData) {
+                  final user = snapshotUser.data!;
+                  return FutureBuilder<List<DeviceSchedule>>(
+                    future: userService.getSchedulerByUserId(userId, true),
+                    builder: (context, snapshotSchedule) {
+                      if (snapshotSchedule.connectionState ==
+                          ConnectionState.waiting) {
+                        return CircularProgressIndicator();
+                      } else if (snapshotSchedule.hasError) {
+                        return Text("Error: ${snapshotSchedule.error}");
+                      } else if (snapshotSchedule.hasData) {
+                        double totalCurrentSpent = snapshotSchedule.data!
+                            .fold(0, (sum, item) => sum + item.current_spent);
+                        double totalExpectedSpent = snapshotSchedule.data!
+                            .fold(0, (sum, item) => sum + item.expected_spent);
+                        return Column(
+                          children: [
+                            Text(
+                              'Total current spent: $totalCurrentSpent',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              'Total expected spent: $totalExpectedSpent',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            SizedBox(height: 20),
+                            Container(
+                              height: MediaQuery.of(context).size.height * 0.6,
+                              child: WeeklySchedule(
+                                schedule: snapshotSchedule.data!,
+                                homeHours: user.home_hours,
+                                homeDuration: user.home_duration,
+                              ),
+                            ),
+                          ],
+                        );
+                      } else {
+                        return Text("No schedule data available");
+                      }
+                    },
+                  );
+                } else {
+                  return Text("No user data available");
+                }
+              },
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(AppThemes.tertiaryColor),
+                  minimumSize: MaterialStateProperty.all<Size>(Size(
+                      MediaQuery.of(context).size.width * 0.3,
+                      MediaQuery.of(context).size.height * 0.08)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.of(context).pushReplacementNamed('/profile');
+                },
+                child: Text('Go to your profile',
+                    style: Theme.of(context).textTheme.bodyLarge),
+              ),
+            ),
+          ],
         ),
-        */
       ),
     );
   }
